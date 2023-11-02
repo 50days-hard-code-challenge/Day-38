8a52ecdfe6aa7c0b75835bad50d5188bd7b40773
YuvikaVarshney <130446237+YuvikaVarshney@users.noreply.github.com>
2023-11-03 00:13:07 +0530

    Create YuvikaVarshney2(sumFirstN).py

---
+++ YuvikaVarshney2(sumFirstN)..py
@@ -0,0 +1,9 @@
+from typing import List
+
+def sumFirstN(n: int) -> int:
+    # Write your code here.
+    return (n*(n+1))//2
+    pass
+    """
+    This was supposed to be done by recursion, but one testcase was going out of the time limit so I decided to just f*ck it and do a easier method
+    """
